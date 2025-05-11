1ce14631d2db3d704c7e4a5482952d8fc2ddff17
Krzysztof Makulewski <knmekowski@gmail.com>
2025-05-11 11:54:47 +0200

    Succesor list - print, edge_exists, BFS, DFS

---
+++ succesor_lists.py
@@ -0,0 +1,78 @@
+from lists import Linked_List
+from checks import check_successor_lists
+
+def succesor_lists_print(succesor_lists: list[Linked_List]) -> None:
+    check_successor_lists(succesor_lists)
+
+    for i in succesor_lists:
+        i.display()
+
+
+def edge_exists_list(succesor_lists: list[Linked_List], start: int, end: int) -> bool:
+    check_successor_lists(succesor_lists)
+
+    if start < 1 or start > len(succesor_lists) or end < 1 or end > len(succesor_lists):
+        raise ValueError("Node values must be between 1 and the number of nodes.")
+
+    return succesor_lists[start - 1].Find(end)
+
+
+def table_BreathFirstSearch(succesor_lists: list[Linked_List], start: int) -> list[int]:
+    check_successor_lists(succesor_lists)
+
+    if start < 1 or start > len(succesor_lists):
+        raise ValueError("Start value must be between 1 and the number of nodes")
+    
+    result = [start]
+    visited = [False] * len(succesor_lists)
+    visited[start - 1] = True
+    i = 0
+
+    while i < len(result):
+        current = succesor_lists[result[i] - 1].head.next
+        while current is not None:
+            if not visited[current.data - 1]:
+                result.append(current.data)
+                visited[current.data - 1] = True
+            current = current.next
+        i += 1
+    
+    return result
+
+
+def table_DepthFirstSearch(succesor_lists: list[Linked_List], start: int) -> list[int]:
+    check_successor_lists(succesor_lists)
+
+    if start < 1 or start > len(succesor_lists):
+        raise ValueError("Start value must be between 1 and the number of nodes")
+    
+    stack = [start]
+    visited = [False] * len(succesor_lists)
+    visited[start - 1] = True
+    result = [start]
+
+    while stack:
+        current = succesor_lists[stack[-1] - 1].head.next
+        while current is not None:
+            if not visited[current.data - 1]:
+                stack.append(current.data)
+                visited[current.data - 1] = True
+                break
+            current = current.next
+        else:
+            stack.pop()
+            result.append(stack[-1])
+    
+    return result
+
+if __name__ == "__main__":
+    succesor_lists = []
+    for i in range(5):
+        linked_list = Linked_List()
+        linked_list.InsertAtEnd(i+1)
+        for j in range(i+1, 5):
+            linked_list.InsertAtEnd(j+1)
+        succesor_lists.append(linked_list)
+    succesor_lists_print(succesor_lists)
+    print(table_BreathFirstSearch(succesor_lists, 1))
+    print(table_DepthFirstSearch(succesor_lists, 1))
